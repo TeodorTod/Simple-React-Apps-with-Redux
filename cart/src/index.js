2f130c1676c7cdbe854ca6c13de0b5ece82911ae
Artos Web <teodor.at.tod@gmail.com>
2022-09-13 22:10:02 +0300

    add Navbar

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { store } from './store'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
