a6d6ca02fa4ddf90c408fe66d394bf1d682f38b8
Artos Web <teodor.at.tod@gmail.com>
2022-09-16 12:00:57 +0300

    ordered posts by date

--- simple-form/src/features/post/PostList.js
+++ simple-form/src/features/post/PostList.js
@@ -1,16 +1,20 @@
 import { useSelector } from "react-redux";
 import PostAuthor from "./PostAuthor";
 import { SelectAllPosts } from "./postSlice";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
     const posts = useSelector(SelectAllPosts);
 
-    const renderedPosts = posts.map(post => (
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+    const renderedPosts = orderedPosts.map(post => (
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
             <p className="postCredit">
                 <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
             </p>
         </article>
     ))
